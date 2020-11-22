97f658e6a49f06bf33788362f0a842f0aa08b93c
RozeFound <RozeFound@gmail.com>
2020-11-23 04:24:45 +0700

    Just changed order

--- src/Utils/Utils.cpp
+++ src/Utils/Utils.cpp
@@ -4,6 +4,50 @@
 
 #pragma warning(disable : 6387)
 
+std::string httpGet(std::string_view url) {
+
+	const enum sizes : unsigned int {
+		Byte = 1,
+		kiloByte = Byte * 1024,
+		megaByte = kiloByte * 1024,
+	};
+
+	winrt::Windows::Web::Http::HttpClient client;
+
+	auto asyncOperation = client.GetAsync(winrt::Windows::Foundation::Uri(winrt::to_hstring(url)));
+	asyncOperation.Progress([=](auto ResponseMessage, winrt::Windows::Web::Http::HttpProgress progressInfo) {
+
+		if (progressInfo.TotalBytesToReceive) {
+
+			float recieved = static_cast<float>(progressInfo.BytesReceived);
+			float total = static_cast<float>(progressInfo.TotalBytesToReceive.Value());
+
+			std::string outString;
+
+			if (total >= sizes::megaByte) fmt::format_to(std::back_inserter(outString), "Recieved {0:.3f}/{1:.3f} MiB", recieved / sizes::megaByte, total / sizes::megaByte);
+			else if (total >= sizes::kiloByte) fmt::format_to(std::back_inserter(outString), "Recieved {0:.3f}/{1:.3f} KiB", recieved / sizes::kiloByte, total / sizes::kiloByte);
+			else fmt::format_to(std::back_inserter(outString), "Recieved {}/{} bytes", recieved, total);
+
+			gui::SetProgressStatic(outString);
+			if (total >= sizes::megaByte * 5) {
+				gui::SetPending(false);
+				gui::SetProgress(progressInfo.BytesReceived, progressInfo.TotalBytesToReceive.Value());
+			}
+		}
+		});
+
+	auto response = asyncOperation.get();
+	winrt::hstring result = response.Content().ReadAsStringAsync().get();
+
+	gui::SetFullProgress();
+	gui::SetProgressStatic("Download Complete.");
+
+	std::this_thread::sleep_for(std::chrono::milliseconds(700));
+	gui::SetPending(true);
+
+	return { std::begin(result), std::end(result) };
+}
+
 bool isPackageExists(std::string_view pkgName) {
 
 	auto packageName = winrt::to_hstring(pkgName);
@@ -38,91 +82,6 @@ std::optional<winrt::Windows::ApplicationModel::Package> getPackageByFamilyName(
 	} while (packages.MoveNext());
 
 	return std::nullopt;
-}
-
-bool CheckCertByThumbPrint(HCERTSTORE hRootCertStore, std::string_view thumbPrint) {
-
-	PCCERT_CONTEXT pCertContext = NULL;
-
-	while (pCertContext = CertEnumCertificatesInStore(hRootCertStore, pCertContext)) {
-
-		DWORD length = 0;
-
-		CertGetCertificateContextProperty(pCertContext, CERT_SHA1_HASH_PROP_ID, nullptr, &length);
-
-		auto buffer = std::make_unique<BYTE[]>(length);
-		if (buffer == nullptr) return false;
-
-		CertGetCertificateContextProperty(pCertContext, CERT_SHA1_HASH_PROP_ID, buffer.get(), &length);
-
-		std::string hexString;
-		boost::algorithm::hex_lower(buffer.get(), std::back_inserter(hexString));
-
-		if (hexString == thumbPrint) return true;
-	}
-	return false;
-}
-
-std::string httpGet(std::string_view url) {
-
-	const enum sizes : unsigned int {
-		Byte = 1,
-		kiloByte = Byte * 1024,
-		megaByte = kiloByte * 1024,
-	};
-
-	winrt::Windows::Web::Http::HttpClient client;
-
-	auto asyncOperation = client.GetAsync(winrt::Windows::Foundation::Uri(winrt::to_hstring(url)));
-	asyncOperation.Progress([=](auto ResponseMessage, winrt::Windows::Web::Http::HttpProgress progressInfo) {
-		
-		if (progressInfo.TotalBytesToReceive) {
-
-			float recieved = static_cast<float>(progressInfo.BytesReceived);
-			float total = static_cast<float>(progressInfo.TotalBytesToReceive.Value());
-
-			std::string outString;
-
-			if (total >= sizes::megaByte) fmt::format_to(std::back_inserter(outString), "Recieved {0:.3f}/{1:.3f} MiB", recieved / sizes::megaByte, total / sizes::megaByte);
-			else if (total >= sizes::kiloByte) fmt::format_to(std::back_inserter(outString), "Recieved {0:.3f}/{1:.3f} KiB", recieved / sizes::kiloByte, total / sizes::kiloByte);
-			else fmt::format_to(std::back_inserter(outString), "Recieved {}/{} bytes", recieved, total);
-
-			gui::SetProgressStatic(outString);
-			if (total >= sizes::megaByte * 5) {
-				gui::SetPending(false);
-				gui::SetProgress(progressInfo.BytesReceived, progressInfo.TotalBytesToReceive.Value());
-			}
-		}
-	});
-
-	auto response = asyncOperation.get();
-	winrt::hstring result = response.Content().ReadAsStringAsync().get();
-
-	gui::SetFullProgress();
-	gui::SetProgressStatic("Download Complete.");
-
-	std::this_thread::sleep_for(std::chrono::milliseconds(700));
-	gui::SetPending(true);
-
-	return {std::begin(result), std::end(result)};
-}
-
-int getOsVersion(void) {
-
-	NTSTATUS(WINAPI * RtlGetVersion)(LPOSVERSIONINFOEXW);
-	OSVERSIONINFOEXW osInfo;
-
-	*(FARPROC*)&RtlGetVersion = GetProcAddress(GetModuleHandleA("ntdll"), "RtlGetVersion");
-
-	if (RtlGetVersion != NULL) {
-
-		osInfo.dwOSVersionInfoSize = sizeof(osInfo);
-		RtlGetVersion(&osInfo);
-
-		return static_cast<int>(osInfo.dwBuildNumber);
-	}
-
-	return 0;
 }
 
 winrt::Windows::Foundation::AsyncStatus installPackageByAppInstallerUrl(std::string_view url) {
@@ -178,30 +137,43 @@ winrt::Windows::Foundation::AsyncStatus installPackageByUrl(std::string_view url
 	return deploymentOperation.Status();
 }
 
-bool installPackageByPath(std::filesystem::path filePath) {
+bool CheckCertByThumbPrint(HCERTSTORE hRootCertStore, std::string_view thumbPrint) {
 
-	//PowerShell.exe -Command "Add-AppxPackage 'path' -AppInstallerFile"
-	std::string command = fmt::format("-Command \"Add-AppxPackage '{}'\"", filePath.string());
+	PCCERT_CONTEXT pCertContext = NULL;
 
-	if (CoInitializeEx(NULL, COINIT_APARTMENTTHREADED | COINIT_DISABLE_OLE1DDE) == S_OK) {
+	while (pCertContext = CertEnumCertificatesInStore(hRootCertStore, pCertContext)) {
 
-		SHELLEXECUTEINFO ShExecInfo;
+		DWORD length = 0;
 
-		ShExecInfo.cbSize = sizeof(SHELLEXECUTEINFO);
-		ShExecInfo.fMask = SEE_MASK_NOCLOSEPROCESS;
-		ShExecInfo.hwnd = NULL;
-		ShExecInfo.lpVerb = L"open";
-		ShExecInfo.lpFile = L"PowerShell.exe";
-		ShExecInfo.lpParameters = winrt::to_hstring(command).c_str();
-		ShExecInfo.lpDirectory = NULL;
-		ShExecInfo.nShow = SW_SHOWDEFAULT;
-		ShExecInfo.hInstApp = NULL;
+		CertGetCertificateContextProperty(pCertContext, CERT_SHA1_HASH_PROP_ID, nullptr, &length);
 
-		ShellExecuteEx(&ShExecInfo);
-		WaitForSingleObject(ShExecInfo.hProcess, INFINITE);
-		CloseHandle(ShExecInfo.hProcess);
+		auto buffer = std::make_unique<BYTE[]>(length);
+		if (buffer == nullptr) return false;
 
-		return true;
+		CertGetCertificateContextProperty(pCertContext, CERT_SHA1_HASH_PROP_ID, buffer.get(), &length);
+
+		std::string hexString;
+		boost::algorithm::hex_lower(buffer.get(), std::back_inserter(hexString));
+
+		if (hexString == thumbPrint) return true;
 	}
 	return false;
+}
+
+int getOsVersion(void) {
+
+	NTSTATUS(WINAPI * RtlGetVersion)(LPOSVERSIONINFOEXW);
+	OSVERSIONINFOEXW osInfo;
+
+	*(FARPROC*)&RtlGetVersion = GetProcAddress(GetModuleHandleA("ntdll"), "RtlGetVersion");
+
+	if (RtlGetVersion != NULL) {
+
+		osInfo.dwOSVersionInfoSize = sizeof(osInfo);
+		RtlGetVersion(&osInfo);
+
+		return static_cast<int>(osInfo.dwBuildNumber);
+	}
+
+	return 0;
 }
--- src/Utils/Utils.hpp
+++ src/Utils/Utils.hpp
@@ -2,12 +2,12 @@
 
 std::string httpGet(std::string_view url);
 
-winrt::Windows::Foundation::AsyncStatus installPackageByAppInstallerUrl(std::string_view url);
-winrt::Windows::Foundation::AsyncStatus installPackageByUrl(std::string_view url);
-[[deprecated]] bool installPackageByPath(std::filesystem::path filePath);
-bool CheckCertByThumbPrint(HCERTSTORE hRootCertStore, std::string_view thumbPrint);
-
 bool isPackageExists(std::string_view pkgName);
 std::optional<winrt::Windows::ApplicationModel::Package> getPackageByFamilyName(std::string_view familyName);
+
+winrt::Windows::Foundation::AsyncStatus installPackageByAppInstallerUrl(std::string_view url);
+winrt::Windows::Foundation::AsyncStatus installPackageByUrl(std::string_view url);
+
+bool CheckCertByThumbPrint(HCERTSTORE hRootCertStore, std::string_view thumbPrint);
 
 int getOsVersion();
